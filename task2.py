13df51c77d40db4fedcd723afe7101f977f7083d
turduhodzhaev99 <turduhodzhaev99@gmail.com>
2020-07-16 20:18:28 +0600

    first commit

---
+++ task2.py
@@ -0,0 +1,25 @@
+"""
+Write a function square_number that
+takes in a number and squares it.
+"""
+#Make sure to un-comment the square_test() line below
+#when you are done.
+#     WRITE YOUR CODE HERE...
+def square_number(number):
+    return number**2
+def biggest_guy(num1, num2, num3):
+    return num1
+  # find bigger guy between num1 and num2
+  # find biggest guy between bigger guy and num3
+print(square_number(10))
+
+#DO NOT remove lines below here,
+#this is designed to test your code.
+def test_square_number():
+    assert square_number(2) == 4
+    assert square_number(8) == 64
+    assert square_number(10) == 100
+    print("YOUR CODE IS CORRECT!")
+#test your code by un-commenting the line(s) below
+test_square_number()
+
